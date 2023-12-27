4d12bb3f23f8ab9527283932e11e54cd4f0c9234
Albert Muller <jzxdrift@users.noreply.github.com>
2023-12-27 15:14:14 -0800

    Add files via upload

---
+++ Card.h
@@ -0,0 +1,11 @@
+#ifndef CARD_H
+#define CARD_H
+
+typedef struct
+{
+	unsigned char frontSide;
+	unsigned char backSide;
+	unsigned hidden;
+} Card;
+
+#endif
---
+++ Utility.h
@@ -0,0 +1,7 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+void getInput(int*);
+void clearScreen(void);
+
+#endif
